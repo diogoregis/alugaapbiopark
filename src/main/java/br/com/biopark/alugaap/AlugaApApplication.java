14164955a947cbb30157f5cfdd08d5170e4e0659
Diogo Regis <diogo.regis.silva@gmail.com>
2023-03-04 17:44:35 -0300

    Star projeto

---
+++ src/main/java/br/com/biopark/alugaap/AlugaApApplication.java
@@ -0,0 +1,13 @@
+package br.com.biopark.alugaap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugaApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugaApApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/biopark/alugaap/AlugaApApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.biopark.alugaap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugaApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
