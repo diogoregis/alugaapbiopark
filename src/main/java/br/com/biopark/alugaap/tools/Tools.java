ac0aef3adf0572076336a4f338a9179107154e51
Diogo Regis <diogo.regis.silva@gmail.com>
2023-03-05 22:12:36 -0300

    add funcao class Tools

--- src/main/java/br/com/biopark/alugaap/tools/Tools.java
+++ src/main/java/br/com/biopark/alugaap/tools/Tools.java
@@ -1,4 +1,15 @@
 package br.com.biopark.alugaap.tools;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Tools {
+
+    static NumberFormat nf = new DecimalFormat("R$ #,##0.00", new DecimalFormatSymbols(new Locale("pt", "BR")));
+    public static String precoFormata(double preco){
+        return Tools.nf.format(preco);
+    }
+
 }
